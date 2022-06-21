a418bcc6102ad4bb972a2519351c1868906a6ab0
Michael Cysouw <cysouw@mac.com>
2022-06-21 11:41:21 +0200

    further cleanup

--- scripts/matchnames.R
+++ scripts/matchnames.R
@@ -1,19 +1,25 @@
-# Rough attempt to match the names of the villages from the printed atlas to the names of the original `Wenkerbögen`. This linking has been manually corrected and enhanced. The final result is documented in the file `data/KDSA_locations.txt`. The current script is only retained here for historical reasons.
+# Rough attempt to match the names of the villages from the printed atlas
+# to the names of the original `Wenkerbögen`.
+# This linking has been manually corrected and enhanced.
+# The final result is documented in the file `data/KDSA_locations.txt`.
+# The current script is only retained here for historical reasons.
 
-ocr <- read.delim("../sources/scans\ from\ printed\ atlas/OCRnames.txt")
-all <- read.delim("../sources/all\ wenker\ places/wenkerorte.txt")
+ocr <- read.delim("../sources/scans/OCRlocations.txt")
+all <- read.delim("../sources/wenkerbogen/wenkerorte.txt")
 
-ocrname <- gsub(" */.+$","",ocr[,3])
-ocrname <- gsub(" *\\(.+$","",ocrname)
+ocrname <- gsub(" */.+$", "", ocr[, 3])
+ocrname <- gsub(" *\\(.+$", "", ocrname)
 
-allname <- gsub(" */.+$","",all$name)
-allname <- gsub(" *\\(.+$","",allname)
+allname <- gsub(" */.+$", "", all$name)
+allname <- gsub(" *\\(.+$", "", allname)
 
-allplace <- gsub(" */.+$","",all$place)
-allplace <- gsub(" *\\(.+$","",allplace)
+allplace <- gsub(" */.+$", "", all$place)
+allplace <- gsub(" *\\(.+$", "", allplace)
 
-m <- sapply(ocrname,function(x){all$bogennummern[allname==x | allplace ==x]})
-l <- sapply(m,length)
-m[l!=1] <- ""
+m <- sapply(ocrname, function(x) {
+  all$bogennummern[allname == x | allplace == x]
+  })
+l <- sapply(m, length)
+m[l != 1] <- ""
 
-write(unlist(m),ncolumns=1,file="../sandbox/match.txt")
+write(unlist(m), ncolumns = 1, file = "../sandbox/match.txt")
